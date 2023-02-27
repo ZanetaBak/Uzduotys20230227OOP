461dd8925b4c0c0485d324a11091c1848c27df84
ZanetaB <zzanule@gmail.com>
2023-02-27 10:56:33 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        public static void main(String[] args) {
+            ArrayList<Book> books = new ArrayList<>();
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Sveiki atvyke i knygyna");
+
+
+
+
+
+
+        }
+
+
+
+
+
+        //=============End of the code============
+    }
+    //====================End of the file ===========
+}
