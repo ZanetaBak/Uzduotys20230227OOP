a71352e168851ecdc137810e8c7e61d065301f5e
ZanetaB <zzanule@gmail.com>
2023-02-28 11:09:20 +0200

    updated code

--- src/Item.java
+++ src/Item.java
@@ -9,6 +9,7 @@ public class Item {
 //price
 //category
 // extra: padaryti switch'e optionus, "pamatyti kategorijų sąrašą" ir "atvaizduoti X kategorijos prekes" */
+    // pasidaryti validacija
 
 public static ArrayList<Item> items = new ArrayList<>();
     private static Item fakeItem = new Item();
@@ -20,7 +21,7 @@ public static ArrayList<Item> items = new ArrayList<>();
     private Double price;
     private String category;
 
-        public Item(){
+        public Item(){ // kiekviena karta sukuriant preke padides idCount
             this.id = idCount++;
         }
     public static void deleteItem (){
@@ -31,19 +32,19 @@ public static ArrayList<Item> items = new ArrayList<>();
 
     public static void editItem(){
         System.out.println("Prekės id");
-        Item x = find(sc.nextInt());
+        Item x = find(sc.nextInt());// irasom knygos, kuria norim surasti id
         System.out.println("Pavadinimas");
         x.title = sc.nextLine();
         sc.nextLine();
         System.out.println("Kaina");
         x.price = sc.nextDouble();
-        sc.nextDouble();
+        sc.nextLine();
         System.out.println("Kategorija");
         x.category = sc.nextLine();
         sc.nextLine();
     }
     public static Item find(int id){
-        for ( Item x: items) {
+        for (Item x: items) {
             if(x.id == id){
                 return x;
             }
@@ -52,31 +53,75 @@ public static ArrayList<Item> items = new ArrayList<>();
     }
 
     public static void createItem(){
-        Scanner sc = new Scanner(System.in);
-        Item x = new Item();
-
+         Item x = new Item();
         System.out.println("Pavadinimas");
         x.title = sc.nextLine();
         sc.nextLine();
         System.out.println("Kaina");
         x.price = sc.nextDouble();
+        sc.nextLine();
         System.out.println("Kategorija");
         x.category = sc.nextLine();
-        items.add(x);
+        sc.nextLine();
+        items.add(x);// pridedam preke i ArrayList masyva
     }
 
     public static void print(){
             for (Item x:items){
                 System.out.println(x);
             }
+
+        // 6 listCategories(); Du ciklai - vienas isrinkti kategorijas is masyvo, o kitas -printinti sarasa
+
+
     }
+
+    public static void categorylist() {
+        System.out.println("Kategorijų sąrašas");
+        ArrayList<String> categoryList = new ArrayList<>();
+        for (Item item : items) {
+                if (!categoryList.contains(item.category)) {
+                    categoryList.add(item.category);
+                }
+            }
+            for (int i = 0; i < categoryList.size(); i++) {
+                System.out.println(categoryList.get(i) + " ");
+            }
+
+        System.out.println();
+    }
+
+
+
+    //                    7.itemsInCategory();
+
+    public static void itemsInCategory() {
+        System.out.println("Pasirinktos kategorijos prekes");
+
+        //for ( Item x: items) {
+        //  if(x.category == id){
+        //    return x.title;
+        //}
+        //}
+        //return fakeItem;
+        // }
+
+    }
+
+    public Item(String title, double price, String category){// default prekes
+            this.id = idCount++;
+            this.title = title;
+            this.price = price;
+            this.category = category;
+}
 
     @Override
     public String toString() {
-        return  "id=" + id +
+        return   id +
                 ". Pavadinimas:'" + title + '\'' +
                 ", kaina: " + price +
-                ", kategorija:" + category + '\'' +
-                '}';
+                ", kategorija: '" + category + '\'';
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Item.items.add(new Item("Kefyras", 1.50, "Pieno produktai"));
+        Item.items.add(new Item("Makaronai", 0.99, "Bakaleja"));
+        Item.items.add(new Item("Saukstas", 0.69, "Stalo irankiai"));
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Sveiki atvykę į parduotuvę!");
         while (true) {
-            info();
-            int input = sc.nextInt();
+            info(); // klausiam pasirinkimo
+            int input = sc.nextInt();// padaryti try catch
             sc.nextLine();
             switch (input) {
                 case 1:
-                    Item.print();
+                    Item.print();// print f-ja kvieciama is Item klases
                     break;
                 case 2:
                     Item.createItem();
@@ -26,18 +29,26 @@ public class Main {
                 case 5:
                     System.exit(1);
                     break;
+                case 6:
+                    Item.categorylist();
+                    break;
+               //ase 7:
+                 // Item.itemsInCategory();
 
             }
+
         }
     }
+// extra: padaryti switch'e optionus, "pamatyti kategorijų sąrašą" ir "atvaizduoti X kategorijos prekes" */
 
+
+
+
+    //=============End of the code============
 
     public static void info() {
         System.out.println("Kokį veiksmą norite atlikti?");
     }
 
-
-    //=============End of the code============
-}
 //====================End of the file ===========
 }
